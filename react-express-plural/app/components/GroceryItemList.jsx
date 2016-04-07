577d8b3e956a22f55599bc339528debb4b6aee02
Cameron Walker-Hebborn <cam4k@outlook.com>
2016-04-07 20:25:26 +0100

    Initial Commit

---
+++ react-express-plural/app/components/GroceryItemList.jsx
@@ -0,0 +1,26 @@
+﻿var React = require('react/addons');
+var GroceryItem = require('./GroceryItem.jsx')
+var GroceryListAddItem = require('./GroceryListAddItem.jsx')
+
+module.exports = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>Grocery Listify</h1>
+                <div>
+                    {
+                        this.props.items.map(function(item, index) {
+                            return(
+                                <GroceryItem key={"item"+index} item={item}/>
+                            )
+                        })
+                    }
+                </div>
+
+                <div>
+                    <GroceryListAddItem />
+                </div>
+            </div>    
+        )
+    }
+});
